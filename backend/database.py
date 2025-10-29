7eebec93bfaf2e9055a65f40c55f2120fee81d96
Ganijon Tashmatov <nephilim0363@gmail.com>
2025-10-29 15:11:38 +0500

    database.py

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import sessionmaker, Session
 from models import Base
 
 # Setup database engine
-ENGINE = create_engine('sqlite:///api.db')
+ENGINE = create_engine("sqlite:////data/api.db")
 
 # Create all tables
 Base.metadata.create_all(ENGINE)
 
 # Session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
+
 
 def get_db():
     """Dependency for getting database sessions"""
